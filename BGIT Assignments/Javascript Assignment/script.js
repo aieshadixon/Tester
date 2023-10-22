ec2f07ffc15b46e07b57ba7b7fdb18c4e9a51f25
aieshadixon <141667863+aieshadixon@users.noreply.github.com>
2023-10-22 23:18:35 +0100

    complete

--- BGIT Assignments/Javascript Assignment/script.js
+++ BGIT Assignments/Javascript Assignment/script.js
@@ -1,17 +1,19 @@
 // Create an array of multiple colors
+
 const colors = ["coral", "red", "green", "blue", "magenta", "yellow"];
 
-let changeTextColor = document.querySelector('changeTextColor');
+// Add an EventListener to the button element
+
+const changeTextColor = document.querySelector('#changeTextColor');
 
 // Add an EventListener to the button element
 changeTextColor.addEventListener("click", function () {
-  const randomColor = getRandomColor();
-  document.body.style.color = randomColor;
+    document.body.style.color = randomColor;
+    let color = "#";
+    for (let i = 0; i < colors.length; i++) {
+      colors += letters [Math.floor(Math.random() * 6)];
+  }
+  return color;
 });
 
-function getRandomColor() {
-  // Generate a random index within the range of the colors array length
-  const randomIndex = Math.floor(Math.random() * colors.length);
-  // Return the color at the randomly generated index
-  return colors[randomIndex];
-}
+  
