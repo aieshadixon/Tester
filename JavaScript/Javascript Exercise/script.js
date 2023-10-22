8053a6402a9f8ac41a966e91e31124c07240d553
aieshadixon <141667863+aieshadixon@users.noreply.github.com>
2023-10-22 23:11:47 +0100

    complete

---
+++ JavaScript/Javascript Exercise/script.js
@@ -0,0 +1,12 @@
+let num1 = 19
+let num2 = 28
+
+sum = num1 + num2;
+difference = num1 - num2;
+product = num1 * num2;
+quotient = num1 / num2;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
